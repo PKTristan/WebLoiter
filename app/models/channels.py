3f05ab0d8c68db17b74d8644c81408cbde488873
collin-smith23 <collinmk@live.com>
2023-05-27 11:09:27 -0600

    basic setup for channels.py db

---
+++ app/models/channels.py
@@ -0,0 +1,14 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from flask_login import UserMixin
+
+class Channel(db.Model, UserMixin):
+    __tablename__ = 'channels'
+
+    if environment == 'production':
+        __table_args__ = {'schema': SCHEMA}
+    
+    id = db.Column(db.Integer, primary_key=True)
+    channel_name = db.Column(db.String(50), nullable=False)
+    server_id = db.Column(db.integer)
+
+    
