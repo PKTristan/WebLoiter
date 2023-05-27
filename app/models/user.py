8f2d0834c75f5e73592d629d06eb8ad5ff350ade
PKTristan <tcalderon1922@gmail.com>
2023-05-27 09:32:02 -0700

    users models finished

--- app/models/user.py
+++ app/models/user.py
@@ -10,9 +10,24 @@ class User(db.Model, UserMixin):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    display_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
+    avi = db.Column(db.String(255), nullable=True)
+    bio = db.Column(db.String(1000), nullable=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+
+    @db.validates('email')
+    def validate_email(self, key, email):
+        if not validate_email_format(email):
+            raise AssertionError('Invalid email format')
+        return email
+
+    @db.validates('avi')
+    def validate_avi(self, key, avi):
+        if not validate_url_format(avi):
+            raise AssertionError('Invalid URL format for avi')
+        return avi
 
     @property
     def password(self):
@@ -31,3 +46,13 @@ class User(db.Model, UserMixin):
             'username': self.username,
             'email': self.email
         }
+
+
+
+def validate_email_format(email) -> bool:
+    # check for valid email
+    return True
+
+def validate_url_format(url) -> bool:
+    # check fro proper url
+    return True
