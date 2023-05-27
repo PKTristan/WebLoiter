75b2d8903c30c5f5a2d7ca49d9dcd61da29176de
collin-smith23 <collinmk@live.com>
2023-05-27 11:57:08 -0600

    channel done

--- app/models/channels.py
+++ app/models/channels.py
@@ -1,4 +1,5 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
 class Channel(db.Model, UserMixin):
@@ -9,6 +10,7 @@ class Channel(db.Model, UserMixin):
     
     id = db.Column(db.Integer, primary_key=True)
     channel_name = db.Column(db.String(50), nullable=False)
-    server_id = db.Column(db.integer)
+    server_id = db.Column(db.integer, db.ForeignKey('servers.id'))
 
-    
+    server = db.relationship('Server', backref=db.backref('channels', lazy=True))
+
