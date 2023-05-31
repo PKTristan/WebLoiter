ffc01a2b9f58dc8ed46b659eff44180ea9831316
PKTristan <tcalderon1922@gmail.com>
2023-05-30 21:29:06 -0700

    added the extra schema stuff to version file

--- migrations/versions/20230530_211047_create_and_update_all_models.py
+++ migrations/versions/20230530_211047_create_and_update_all_models.py
@@ -8,6 +8,9 @@ Create Date: 2023-05-30 21:10:47.922055
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = '0240d6c0e1d2'
@@ -62,6 +65,10 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+
+
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
