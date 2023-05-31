2e92ca630d545480921c7d03ecdfaaab44271b5e
agabriele73 <agabrielejr@gmail.com>
2023-05-30 21:36:10 -0700

    added SCHEMA back to all models

--- app/models/channelmembers.py
+++ app/models/channelmembers.py
@@ -8,8 +8,8 @@ class ChannelMembers(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    member_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    channel_id = db.Column(db.Integer, db.ForeignKey('channels.id'))
+    member_id = db.Column(db.Integer, db.ForeignKey('{SCHEMA}.users.id'))
+    channel_id = db.Column(db.Integer, db.ForeignKey('{SCHEMA}.channels.id'))
 
     user = db.relationship('User', backref='channelmembers')
     channel = db.relationship('Channel', backref='channelmembers')
