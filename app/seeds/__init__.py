44973bfffdf013ba39d838f5c50254f0bbe77c04
Alfonso Gabriel <104112395+agabriele73@users.noreply.github.com>
2023-05-31 21:00:13 -0700

    Revert "Fixing indentation issues"

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,9 +1,5 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
-from .servermembers import seed_server_members, undo_server_members
-from .channelmembers import seed_channel_members, undo_channel_members
-from .servers import seed_servers, undo_servers
-from .channels import seed_channels, undo_channels
 
 from app.models.db import db, environment, SCHEMA
 
@@ -16,21 +12,13 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
-        # Before seeding in production, you want to run the seed undo
-        # command, which will  truncate all tables prefixed with
+        # Before seeding in production, you want to run the seed undo 
+        # command, which will  truncate all tables prefixed with 
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
         undo_users()
-        undo_channel_members()
-        undo_server_members()
-        undo_servers()
-        undo_channels()
     seed_users()
     # Add other seed functions here
-    seed_channel_members()
-    seed_server_members()
-    seed_servers()
-    seed_channels()
 
 
 # Creates the `flask seed undo` command
@@ -38,8 +26,3 @@ def seed():
 def undo():
     undo_users()
     # Add other undo functions here
-    undo_channel_members()
-    undo_server_members()
-    undo_servers()
-    undo_channels()
-
