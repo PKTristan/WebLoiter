fa9082971fde04dbd2ab180731489f61f25dc632
agabriele73 <agabrielejr@gmail.com>
2023-06-01 20:21:36 -0700

    adding-production-to-versions

--- migrations/versions/20230601_211308_.py
+++ migrations/versions/20230601_211308_.py
@@ -7,7 +7,9 @@ Create Date: 2023-06-01 21:13:08.846041
 """
 from alembic import op
 import sqlalchemy as sa
-
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = 'e049fc2afd16'
@@ -72,6 +74,8 @@ def upgrade():
                existing_nullable=False)
 
     # ### end Alembic commands ###
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};") 
 
 
 def downgrade():
