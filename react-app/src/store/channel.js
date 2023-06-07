83e89c833d345d79e1cb5f4081f5d6014ae3e9b0
Tristan C <47341361+PKTristan@users.noreply.github.com>
2023-06-06 20:00:36 -0700

    finished channel store

---
+++ react-app/src/store/channel.js
@@ -0,0 +1,85 @@
+
+
+
+const LOAD_CHANNELS = "channels/LOAD_CHANNELS";
+const LOAD_CHANNEL_BY_ID = "channels/LOAD_CHANNEL_BY_ID";
+const LOAD_NEW_ID = "channels/LOAD_NEW_ID";
+
+const loadChannels = (channels) => ({
+    type: LOAD_CHANNELS,
+    channels
+});
+
+const loadChannelById = (channel) => {
+    type: LOAD_CHANNEL_BY_ID,
+    channel
+}
+
+const loadNewId = (id) => ({
+    type: LOAD_NEW_ID,
+    id
+})
+
+export const getChannelsByServer = (serverId) => async (dispatch) => {
+    const response = await fetch(`/api/servers/${serverId}/channels`);
+
+    if (response.ok) {
+        const channels = await response.json();
+        dispatch(loadChannels(channels));
+    }
+
+    return response;
+}
+
+export const getChannelById = (id) => async (dispatch) => {
+    const response = await fetch(`/api/channels/${id}`);
+
+    if (response.ok) {
+        const channel = await response.json();
+        dispatch(loadChannelById(channel));
+    }
+}
+
+export const createChannel = (channel) => async (dispatch) => {
+    const response = await fetch('/api/channels', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(channel)
+    });
+
+    if (response.ok) {
+        const newChannel = await response.json();
+        dispatch(loadNewId(newChannel.id));
+    }
+}
+
+export const editChannel = (channel, id) => async (dispatch) => {
+
+}
+
+const initialState = {channels: null, channel: null, newId: null}
+
+const channelsReducer = (state = initialState, action) => {
+    let mutState = Object.assign(state);
+
+    switch (action.type) {
+        case LOAD_CHANNELS:
+
+            return {...mutState, channels: action.channels}
+
+        case LOAD_CHANNEL_BY_ID:
+
+            return {...mutState, channel: action.channel}
+
+        case LOAD_NEW_ID:
+
+            return {...mutState, newId: action.id}
+
+        default:
+            return state;
+    }
+}
+
+export default channelsReducer;
