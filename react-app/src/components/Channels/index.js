44d1777c0ca8566258f81c004751d445bf302344
Tristan C <47341361+PKTristan@users.noreply.github.com>
2023-06-07 18:08:16 -0700

    finished channels navbar

---
+++ react-app/src/components/Channels/index.js
@@ -0,0 +1,44 @@
+import {useSelector} from 'react-redux';
+import { useState, useEffect } from 'react';
+import { selChannels } from '../../store/channel';
+import {useHistory} from 'react-router-dom';
+
+function Channels() {
+    const serverChannels = useSelector(selChannels);
+    const [channels, setChannels] = useState([]);
+    const history = useHistory();
+
+
+    useEffect(() => {
+        if (serverChannels.length > 0) {
+            setChannels(serverChannels);
+        }
+    }, [serverChannels]);
+
+    const handleClick = (e, channel) => () => {
+        e.preventDefault();
+        summonPage(channel.id)
+    }
+
+    const summonPage = (id) => {
+        history.push(`/channels/${id}`);
+    }
+
+    const newChannel = (e) => {
+        e.preventDefault();
+        history.push('/channels/new');
+    }
+
+    return (
+        <section className="channels">
+            {
+                channels.length &&
+                channels.map((channel) => (
+                    <button key={channel.id} className="channel-button" onClick={handleClick(e, channel)}>{`# ${channel.name}`}</button>
+                ))
+            }
+
+            <button className="new-channel-button" onClick={newChannel(e)}>+</button>
+        </section>
+    )
+}
