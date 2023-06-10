d1375cf7b543aa5d9d472749cf3d748cae13af28
agabriele73 <agabrielejr@gmail.com>
2023-06-10 12:02:01 -0700

    adding my image to founders home page

--- react-app/src/components/Homepage/Founders.js
+++ react-app/src/components/Homepage/Founders.js
@@ -6,10 +6,10 @@ function Founders() {
     return (
         <div className="founders-cards">
             <div className="alfonso">
-                <img src={userImg} alt="alfonso" />
+                <img src='https://i.imgur.com/5T32dlj.jpg' alt="alfonso" style={{borderRadius: '50%', height: '500px', width: '400px'}}/>
                 <h4>Alfonso</h4>
                 <p>
-                    Literal founder right here, girlypops.
+                    Literal founder right here, Top G.
                 </p>
             </div>
 
