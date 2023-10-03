c20d07fda9d1d560d0b1ed5ca1d9bacd6addd5e1
agabriele73 <agabrielejr@gmail.com>
2023-10-02 20:34:46 -0700

    changed home page and added links properly

--- react-app/src/components/Homepage/index.js
+++ react-app/src/components/Homepage/index.js
@@ -2,12 +2,20 @@ import React from "react";
 import About from "./About";
 import HowTo from "./HowTo";
 import "./HomePage.css";
+import Founders from "./Founders";
 
 function Homepage() {
     return (
         <div className="homepage-wrapper">
-            <About />
-            <HowTo />
+            <div className="homepage-content">
+                <About />
+            </div>
+            <div className="homepage-content">
+                <HowTo />
+            </div>
+            <div className="homepage-content">
+                <Founders />
+            </div>
         </div>
     )
 }
